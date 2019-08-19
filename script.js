80771ef57d1ceee615011b39a6ff062aae6f733f
Kristi Byrnes <kristi.m.byrnes@accenture.com>
2019-08-19 12:03:28 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.lineJoin = 'butt';
+ctx.lineCap = 'square';
+ctx.lineWidth = 0;
+
+//flag if drawing
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e){
+	if (!isDrawing) return; //stop the fn from running when they are not moused down
+
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	//start from
+	ctx.moveTo(lastX, lastY);
+	//go to
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+	hue++;
+
+	if(ctx.lineWidth >= 70 || ctx.lineWidth <= 1) {
+		direction = !direction;
+	}
+
+	if(direction){
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
+}
+
+canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
